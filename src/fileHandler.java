09b5c27c18be46bc8b924bf4f39f07f1dfb67937
anushatalasila <talasila.a@husky.neu.edu>
2020-04-11 00:28:58 -0400

    Create fileHandler.java

---
+++ src/fileHandler.java
@@ -0,0 +1,23 @@
+
+import 	java.io.File;
+import java.io.FileNotFoundException;
+//Import the Scanner class to read text files
+import java.util.Scanner;
+
+public class fileHandler {
+	public static void main(String[] args) {
+		try {
+		// Creating an object of the file for reading the data
+		File myObj = new File(".txt");  
+		Scanner myReader = new Scanner(myObj);
+		while (myReader.hasNextLine()) {
+		String data = myReader.nextLine();
+		System.out.println(data);
+		}
+		myReader.close();
+		} catch (FileNotFoundException e) {
+		System.out.println("An error occurred.");
+		e.printStackTrace();
+		}
+		}
+}
