56e3df8efb7b992757b4eecb3cba1d847d079b3b
Somanwita <somanwita21@gmail.com>
2020-04-15 16:02:28 -0400

    RankingSystem

--- src/team.java
+++ src/team.java
@@ -1,17 +1,9 @@
 public class team {
 	private String teamName;
-	//private int noOfMatchesWon;
-	//private int noOfGoals;
-	//private boolean homeGround;
-	private int totalNoOfMatches;
-	private int totalmerginOfVictory;
-	private int curmerginOfVictory;
-	private int avgmarginOfVictory;
-	private int maxmarginOfVictory;
-	private int totalmerginOfLoss;
-	private int curmarginOfLoss;
-	private int avgmarginOfLoss;
-	private int maxmarginOfLoss;
+	private int noOfMatchesWon;
+	private int noOfGoals;
+	private boolean homeGround;
+
 
 	public team() {
 		super();
@@ -21,26 +13,6 @@ public class team {
 	public team(String teamName) {
 		super();
 		this.teamName = teamName;
-	}
-
-
-	public team(String teamName, int curmerginOfVictory, int curmarginOfLoss) {
-		super();
-		this.teamName = teamName;
-		this.curmerginOfVictory = curmerginOfVictory;
-		this.curmarginOfLoss = curmarginOfLoss;
-	}
-	
-	public void addValue(int curmerginOfVictory, int curmarginOfLoss) {
-		totalNoOfMatches += 1;
-		
-		totalmerginOfVictory += curmerginOfVictory;
-		avgmarginOfVictory = totalmerginOfVictory/totalNoOfMatches;
-		maxmarginOfVictory = Math.max(maxmarginOfVictory, curmerginOfVictory);
-		
-		totalmerginOfLoss += curmarginOfLoss;
-		avgmarginOfLoss = curmarginOfLoss/totalNoOfMatches;
-		maxmarginOfLoss = Math.max(maxmarginOfLoss, curmarginOfLoss);
 	}
 
 
@@ -54,95 +26,36 @@ public class team {
 	}
 
 
-	public int getTotalNoOfMatches() {
-		return totalNoOfMatches;
+	public int getNoOfMatchesWon() {
+		return noOfMatchesWon;
 	}
 
 
-	public void setTotalNoOfMatches(int totalNoOfMatches) {
-		this.totalNoOfMatches = totalNoOfMatches;
+	public void setNoOfMatchesWon(int noOfMatchesWon) {
+		this.noOfMatchesWon = noOfMatchesWon;
 	}
 
 
-	public int getTotalmerginOfVictory() {
-		return totalmerginOfVictory;
+	public int getNoOfGoals() {
+		return noOfGoals;
 	}
 
 
-	public void setTotalmerginOfVictory(int totalmerginOfVictory) {
-		this.totalmerginOfVictory = totalmerginOfVictory;
+	public void setNoOfGoals(int noOfGoals) {
+		this.noOfGoals = noOfGoals;
 	}
 
 
-	public int getCurmerginOfVictory() {
-		return curmerginOfVictory;
+	public boolean isHomeGround() {
+		return homeGround;
 	}
 
 
-	public void setCurmerginOfVictory(int curmerginOfVictory) {
-		this.curmerginOfVictory = curmerginOfVictory;
+	public void setHomeGround(boolean homeGround) {
+		this.homeGround = homeGround;
 	}
 
-
-	public int getAvgmarginOfVictory() {
-		return avgmarginOfVictory;
-	}
-
-
-	public void setAvgmarginOfVictory(int avgmarginOfVictory) {
-		this.avgmarginOfVictory = avgmarginOfVictory;
-	}
-
-
-	public int getMaxmarginOfVictory() {
-		return maxmarginOfVictory;
-	}
-
-
-	public void setMaxmarginOfVictory(int maxmarginOfVictory) {
-		this.maxmarginOfVictory = maxmarginOfVictory;
-	}
-
-
-	public int getTotalmerginOfLoss() {
-		return totalmerginOfLoss;
-	}
-
-
-	public void setTotalmerginOfLoss(int totalmerginOfLoss) {
-		this.totalmerginOfLoss = totalmerginOfLoss;
-	}
-
-
-	public int getCurmarginOfLoss() {
-		return curmarginOfLoss;
-	}
-
-
-	public void setCurmarginOfLoss(int curmarginOfLoss) {
-		this.curmarginOfLoss = curmarginOfLoss;
-	}
-
-
-	public int getAvgmarginOfLoss() {
-		return avgmarginOfLoss;
-	}
-
-
-	public void setAvgmarginOfLoss(int avgmarginOfLoss) {
-		this.avgmarginOfLoss = avgmarginOfLoss;
-	}
-
-
-	public int getMaxmarginOfLoss() {
-		return maxmarginOfLoss;
-	}
-
-
-	public void setMaxmarginOfLoss(int maxmarginOfLoss) {
-		this.maxmarginOfLoss = maxmarginOfLoss;
-	}
-
+	
 
 
 
