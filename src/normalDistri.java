d4dea231e55451c3f41b2aae66442743db81af24
anushatalasila <talasila.a@husky.neu.edu>
2020-04-17 17:08:43 -0400

    Normal Disrtibution Value calculation

--- src/normalDistri.java
+++ src/normalDistri.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class normalDistri {
 //private double mean;
@@ -15,20 +16,32 @@ public normalDistri() {
 public double calculateND(double normalVari, double mean, double sd) {
 	double value = 0;
 	double temp = Math.pow((normalVari-mean), 2);
-	double total = temp/(2*sd*sd);
+	double total = -temp/(2*sd*sd);
 	
 	value = (1/(sd* Math.sqrt(2*3.14159)))* (Math.pow(2.71828, total));
 	return value;
 }
+public double mean (List<Double> list) {
+	double sum = 0.0;
+	double mean = 0.0;
+	for(int i = 0; i<list.size();i++) {
+		sum += (list.get(i));	
+		}
+	
+	mean = sum/list.size();
+	return mean;
+}
 
-public double standardDeviation(double array[], double mean) {
-	double sd = 0;
-	double sqrDiff = 0;
-	for(int i = 0; i<array.length;i++) {
-	sqrDiff += (array[i] - mean)* (array[i] -mean);	
+public double standardDeviation(List<Double> list, double mean) {
+	double sd = 0.0;
+	double sqrDiff = 0.0;
+	for(int i = 0; i<list.size();i++) {
+	sqrDiff += ((list.get(i) - mean) * (list.get(i) - mean));	
 	}
-	double variance = sqrDiff/(array.length+1);
+	double variance = sqrDiff/(list.size());
 	sd = Math.sqrt(variance);
 	return sd;
 }
+
+
 }
